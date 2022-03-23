eb34a757eadba8a97c2a0adc2026c864e171a0dd
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-23 19:18:33 -0300

    feat: add realtime database

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -9,19 +9,20 @@ export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
-    firestore()
+    const subscribe = firestore()
       .collection('products')
-      .get()
-      .then((response) => {
-        const data = response.docs.map((doc) => {
+      .onSnapshot((querySnapShot) => {
+        const data = querySnapShot.docs.map((doc) => {
           return {
             id: doc.id,
             ...doc.data(),
           };
         }) as ProductProps[];
+
         setProducts(data);
-      })
-      .catch((error) => console.log(error));
+      });
+
+    return () => subscribe();
   }, []);
 
   return (
