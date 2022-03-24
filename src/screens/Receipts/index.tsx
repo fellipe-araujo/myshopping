26f7a30ecfb4139655b1ed61bf89d63380b16a13
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-23 22:11:41 -0300

    feat: get image url from file

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+import storage from '@react-native-firebase/storage';
 
 import { Container, PhotoInfo } from './styles';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { File, FileProps } from '../../components/File';
 
 export function Receipts() {
+  const [photos, setPhotos] = useState<FileProps[]>([]);
+  const [photoSelected, setPhotoSelected] = useState('');
+
+  async function handleShowImage(path: string) {
+    const urlImage = await storage().ref(path).getDownloadURL();
+    setPhotoSelected(urlImage);
+  }
+
+  useEffect(() => {
+    storage()
+      .ref('images')
+      .list()
+      .then((result) => {
+        let files: FileProps[] = [];
+
+        result.items.forEach((file) => {
+          files.push({
+            name: file.name,
+            path: file.fullPath,
+          });
+        });
+
+        setPhotos(files);
+      });
+  }, []);
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
-      <Photo uri="" />
+      <Photo uri={photoSelected} />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>Informações da foto</PhotoInfo>
 
       <FlatList
-        data={photosData}
-        keyExtractor={item => item.name}
+        data={photos}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
           <File
             data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
+            onShow={() => handleShowImage(item.path)}
+            onDelete={() => {}}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
