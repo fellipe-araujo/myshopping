e18773e36f99a373f01a2a34baf9e491e08a288b
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-23 21:56:41 -0300

    feat: create feature of upload images

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
@@ -9,6 +11,8 @@ import { Container, Content, Progress, Transferred } from './styles';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -24,27 +28,46 @@ export function Upload() {
         setImage(result.uri);
       }
     }
-  };
+  }
+
+  async function handleUpload() {
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/images/${fileName}.png`);
+
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on('state_changed', (taskSnapshot) => {
+      const percent = (
+        (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *
+        100
+      ).toFixed(0);
+
+      setProgress(percent);
+
+      setBytesTransferred(
+        `${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`
+      );
+    });
+
+    uploadTask
+      .then(() => {
+        Alert.alert('Upload concluído com sucesso!');
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
-        <Button
-          title="Fazer upload"
-          onPress={() => { }}
-        />
+        <Button title="Fazer upload" onPress={handleUpload} />
 
-        <Progress>
-          0%
-        </Progress>
+        <Progress>{progress}%</Progress>
 
-        <Transferred>
-          0 de 100 bytes transferido
-        </Transferred>
+        <Transferred>{bytesTransferred}</Transferred>
       </Content>
     </Container>
   );
