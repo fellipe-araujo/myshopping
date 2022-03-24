154f64ac0d7f9576a46c5bab742d561b11080781
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-24 16:10:57 -0300

    feat: add others authentication methods

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignInAnonymously() {
+    const { user } = await auth().signInAnonymously();
+  }
+
+  async function handleCreateUserAccount() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Usuário criado com sucesso!');
+      })
+      .catch((error) => {
+        console.log(error.code);
+
+        if (error.code === 'auth/email-already-in-use') {
+          return Alert.alert(
+            'E-mail não disponível. Escolha outro e-mail para cadastrar!'
+          );
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('E-mail inválido!');
+        }
+
+        if (error.code === 'auth/weak-password') {
+          return Alert.alert('A senha dee ter no mínimo 6 dígitos!');
+        }
+      });
+  }
+
+  function handleSignInWithEmailAndPassword() {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => console.log(user))
+      .catch((error) => {
+        console.log(error);
+
+        if (
+          error.code === 'auth/user-not-found' ||
+          error.code === 'auth/wrong-password'
+        ) {
+          Alert.alert('Usuário não encontrado. E-mail e/ou senha inválida!');
+        }
+      });
+  }
+
+  function handleForgotPassword() {
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() =>
+        Alert.alert(
+          'Enviamos um link para seu e-mail para que você possa redefinir sua senha.'
+        )
+      );
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +74,19 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+      <Input placeholder="senha" secureTextEntry onChangeText={setPassword} />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleForgotPassword} />
+        <ButtonText
+          title="Criar minha conta"
+          onPress={handleCreateUserAccount}
+        />
       </Account>
     </Container>
   );
